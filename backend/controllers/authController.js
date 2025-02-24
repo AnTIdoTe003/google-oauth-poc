ba1a979deed1d00d7789a771f87ccb46c71b039f
Debmalya Biswas <viperbale.db@gmail.com>
2025-02-24 18:00:59 +0530

    added client id

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,7 +1,9 @@
 const jwt = require("jsonwebtoken");
 const { OAuth2Client } = require("google-auth-library");
 const User = require("../models/User");
-const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+const client = new OAuth2Client(
+  "810546057582-dekmu2djvt1n09fom490vbl89buj1n6i.apps.googleusercontent.com"
+);
 
 const generateToken = (user) => {
   return jwt.sign({ id: user._id }, "GoogleOAuthPoc", {
@@ -15,7 +17,8 @@ exports.googleLogin = async (req, res) => {
     const { token } = req.body;
     const ticket = await client.verifyIdToken({
       idToken: token,
-      audience: process.env.GOOGLE_CLIENT_ID,
+      audience:
+        "810546057582-dekmu2djvt1n09fom490vbl89buj1n6i.apps.googleusercontent.com",
     });
 
     const { email, name, sub: googleId } = ticket.getPayload();
